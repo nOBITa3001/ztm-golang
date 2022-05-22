a5254015ffb713fc36603b88185a709c379cf558
Pongsakorn Thanopassakul <iampossible3001@gmail.com>
2022-05-22 18:49:50 +0200

    arrays exercise

--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,6 +18,37 @@ package main
 
 import "fmt"
 
-func main() {
+type Product struct {
+	name  string
+	price int
+}
 
+func printShoppingListInfo(list [4]Product) {
+	var cost, totalItem int
+
+	for i := 0; i < len(list); i++ {
+		item := list[i]
+		cost += item.price
+		if item.name != "" {
+			totalItem += 1
+		}
+	}
+
+	fmt.Println("The last item on the list:", list[totalItem-1])
+	fmt.Println("The total number of items:", totalItem)
+	fmt.Println("The total cost of the items:", cost)
+}
+
+func main() {
+	shoppingList := [4]Product{
+		{name: "Banana", price: 1},
+		{name: "Meat", price: 6},
+		{name: "Salad", price: 3},
+	}
+
+	printShoppingListInfo(shoppingList)
+
+	shoppingList[3] = Product{name: "Bread", price: 4}
+
+	printShoppingListInfo(shoppingList)
 }
