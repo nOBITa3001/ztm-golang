c9a2be7ad9520f4e966f10bf3f8b984941db8a69
Pongsakorn Thanopassakul <iampossible3001@gmail.com>
2022-05-22 17:57:41 +0200

    switch exercise

--- src/lectures/exercise/switch/switch.go
+++ src/lectures/exercise/switch/switch.go
@@ -14,4 +14,16 @@ package main
 import "fmt"
 
 func main() {
+	switch age := 0; {
+	case age < 1:
+		fmt.Println("newborn")
+	case age < 4:
+		fmt.Println("toddle")
+	case age < 13:
+		fmt.Println("child")
+	case age < 18:
+		fmt.Println("teenager")
+	default:
+		fmt.Println("adult")
+	}
 }
